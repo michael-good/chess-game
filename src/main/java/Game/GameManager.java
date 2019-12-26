34010961e637c9259d12af5401bba8a25653b316
michael-good <buenosnchez.miguelangelbs.migu@gmail.com>
2019-12-26 11:09:17 +0100

    Added more functionality to UI and started to implement GameManager. Letter of Knight changed to N

--- src/main/java/Game/GameManager.java
+++ src/main/java/Game/GameManager.java
@@ -1,5 +1,6 @@
 package Game;
 
+import Exceptions.InvalidMoveException;
 import Pieces.Piece;
 
 public class GameManager {
@@ -14,8 +15,32 @@ public class GameManager {
         return game;
     }
 
-    public void movePiece(Piece piece, Coordinate source, Coordinate dest) {
+    public void movePiece(Coordinate source, Coordinate dest) throws InvalidMoveException {
+        Piece sourcePiece = game.getBoard().getSquare(source).getPiece();
 
+        // Check source is valid
+        if(sourcePiece == null)
+            throw new InvalidMoveException("There is no piece on the source square provided... Please, try again");
+        if(sourcePiece.getColor() != game.getTurn().getColor())
+            throw new InvalidMoveException("Selected piece does not belong to you... Please, try again");
+
+        // Check if the movement is valid
+        boolean isValidMovement = sourcePiece.isValidMovement(source, dest);
+        Coordinate[] path = sourcePiece.getPath(source, dest);
+        for(Coordinate coord : path) {
+            if(game.getBoard().getSquare(coord).isOccupied() &&
+                    !game.getBoard().getSquare(coord).getPiece().equals(sourcePiece)) {
+                throw new InvalidMoveException("Wrong move... Please, try again");
+            }
+        }
+
+        if(isValidMovement) {
+            game.getBoard().getSquare(source).releaseSquare();
+            if(!game.getBoard().getSquare(dest).isOccupied())
+                game.getBoard().getSquare(dest).setPiece(sourcePiece);
+        } else {
+            throw new InvalidMoveException("Movement is not valid for the selected piece... Please, try again");
+        }
     }
 
     public boolean checkForCheck() {
--- src/main/java/Game/UI.java
+++ src/main/java/Game/UI.java
@@ -9,12 +9,13 @@ public class UI {
     private static String namePlayer2;
 
     public static void main(String[] args) {
-        String namePlayer1, namePlayer2, move, source_piece;
+        String namePlayer1, namePlayer2, moveSource, moveDest, sourcePiece;
         Scanner in = new Scanner(System.in);
         InputParser inputParser = new InputParser();
         Coordinate dest = null;
         Coordinate source = null;
         Player winner = null;
+        boolean notValidMove = true;
 
         System.out.println("Welcome to Chess Game!" + "\n");
         System.out.println("Input name of first player: ");
@@ -27,31 +28,29 @@ public class UI {
         gameManager.getGame().startGame();
         gameManager.getGame().getBoard().printBoard();
         System.out.println("\n" + gameManager.getGame().getTurn().getName() +
-                " starts playing! Select a piece to move in the 'A5' format:");
+                " starts playing!");
 
         while(winner == null) {
-            while(source == null) {
-                source_piece = in.nextLine();
+
+            while(notValidMove) {
+                System.out.println("\n" + gameManager.getGame().getTurn().getName() +
+                        ", select a piece to move in the 'A5' format:");
+                moveSource = in.nextLine();
+                System.out.println("\n" + gameManager.getGame().getTurn().getName() +
+                        ", make a move in the 'A5' format:");
+                moveDest = in.nextLine();
                 try {
-                    source = inputParser.parseInputMove(source_piece);
-                } catch (InvalidMoveException e) {
-                    System.err.println(e.getMessage());
-                }
-            }
-            System.out.println("\n" + gameManager.getGame().getTurn().getName() +
-                    ", make a move in the 'A5' format:");
-            while(dest == null) {
-                move = in.nextLine();
-                try {
-                    dest = inputParser.parseInputMove(move);
+                    source = inputParser.parseInputMove(moveSource);
+                    dest = inputParser.parseInputMove(moveDest);
+                    gameManager.movePiece(source, dest);
+                    notValidMove = false;
                 } catch (InvalidMoveException e) {
                     System.err.println(e.getMessage());
                 }
             }
             gameManager.getGame().passTurn();
             gameManager.getGame().getBoard().printBoard();
-            System.out.println("\n" + gameManager.getGame().getTurn().getName() +
-                    ", select a piece to move in the 'A5' format:");
+            notValidMove = true;
             source = null;
             dest = null;
         }
